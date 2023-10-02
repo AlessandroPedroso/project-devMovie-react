d67a68c26fd611d26b4609f6163a6f72f009f737
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2023-10-02 12:03:36 -0300

    alteração do scroll

--- src/components/Slider/styles.js
+++ src/components/Slider/styles.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div `
-background: #000;
-padding: 0 20px;
+    background: #000;
+    padding: 0 20px;
+    overflow: hidden;
 
     .swiper-wrapper{
         display: flex;
